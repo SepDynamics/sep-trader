4b6049b6d39ec0de88655fa5f9430ba021e59942
scrallex <56850857+scrallex@users.noreply.github.com>
2025-08-24 09:14:50 -0500

    Move test data into testbed

--- _sep/testbed/tests/signal_pipeline_test.cpp
+++ _sep/testbed/tests/signal_pipeline_test.cpp
@@ -7,7 +7,7 @@
 #include "util/nlohmann_json_safe.h"
 
 int main() {
-    std::ifstream file("assets/test_data/eur_usd_m1_48h.json");
+    std::ifstream file("_sep/testbed/test_data/eur_usd_m1_48h.json");
     if (!file.is_open()) return 1;
     nlohmann::json j; file >> j;
     std::vector<double> prices;
